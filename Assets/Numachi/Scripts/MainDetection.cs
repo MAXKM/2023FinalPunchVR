0b40e6b6c86bba7c07efb9251d8cb8e804d6dd81
Numachi0622 <122448310+Numachi0622@users.noreply.github.com>
2023-09-27 20:26:34 +0900

    コライダーの有効化/無効化を実装

--- Assets/Numachi/Scripts/MainDetection.cs
+++ Assets/Numachi/Scripts/MainDetection.cs
@@ -9,6 +9,10 @@ public class MainDetection : MonoBehaviour
     private Vector3 direction; // 残像の方向ベクトル
     [SerializeField] private AfterimageManager afterimageManager;
     [SerializeField] private AppearAfterimage appearAfterimage;
+    [SerializeField] private Collider[] handCollider; // 0=>左 1=>右
+
+    // テスト用
+    [SerializeField] private bool isVRTest;
     private void OnTriggerEnter(Collider other)
     {
         if (inFrontHitPos == null) return;
@@ -18,6 +22,17 @@ public class MainDetection : MonoBehaviour
         Quaternion targetRot = Quaternion.FromToRotation(transform.forward,direction);
         appearAfterimage.Appear(inFrontHitPos,targetRot);
         afterimageManager.Store(inFrontHitPos,targetRot);
+        if (!isVRTest) return; // テスト用、後で消す
+        if (other.CompareTag("LeftHand"))
+        {
+            handCollider[0].enabled = false;
+            handCollider[1].enabled = true;
+        }
+        else
+        {
+            handCollider[0].enabled = true;
+            handCollider[1].enabled = false;
+        }
     }
 
     public void SetInFrontPosition(Vector3 _pos)
