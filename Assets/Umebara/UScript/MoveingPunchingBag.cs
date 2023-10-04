d8d121b476550b2b86fdef2fb68ff1b410267c6f
Kota <Nakoichi39@outlook.jp>
2023-10-04 18:34:28 +0900

    スコア

--- Assets/Umebara/UScript/MoveingPunchingBag.cs
+++ Assets/Umebara/UScript/MoveingPunchingBag.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 
 public class MoveingPunchingBag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -19,8 +12,7 @@ public class MoveingPunchingBag : MonoBehaviour
             KnockBack();
         }
     }
-
-    void KnockBack()
+    public void KnockBack()
     {
         this.transform.DOMove(new Vector3(0f, 0f, 1.75f), 0.075f).SetLoops(2, LoopType.Yoyo);
         this.transform.DORotate(new Vector3(45f, 0f, 0f), 0.075f).SetLoops(2, LoopType.Yoyo);
